d1cf52bede920b52c76c2907ec548dc60f96b433
subswai <swain.subhasis@gmail.com>
2020-08-16 18:12:25 +0100

    vix digital task

--- digital-service-provider/digital-service-api/src/test/java/com/vix/digital/services/online/DigitalServiceProviderApplicationTests.java
+++ digital-service-provider/digital-service-api/src/test/java/com/vix/digital/services/online/DigitalServiceProviderApplicationTests.java
@@ -1,13 +1,21 @@
 package com.vix.digital.services.online;
 
+import com.vix.digital.services.online.controller.DigitalServiceController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest
 class DigitalServiceProviderApplicationTests {
 
+	@Autowired
+	DigitalServiceController controller;
+
 	@Test
-	void contextLoads() {
+	public void assertThatAppContextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
